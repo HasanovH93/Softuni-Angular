93b045aa91b0fe9ea2e39dc535c2445ae9e44c62
HasanovH93 <hasanovh14@gmail.com>
2022-11-01 12:39:22 +0100

    typescript demo

---
+++ Typescript/demo1/index.js
@@ -0,0 +1,8 @@
+var isDone = false;
+var decima = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var message = "";
+var messages = ['Message 1', 'Message 2'];
+var strictMessage = ['Message 1', 2.3];
---
+++ Typescript/demo1/index.ts
@@ -0,0 +1,12 @@
+let isDone: boolean = false;
+
+let decima: number = 6;
+
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+let message: string = "";
+
+let messages: string[] = ['Message 1', 'Message 2'];
+let strictMessage: ['Message 1', number] = ['Message 1', 2.3]
