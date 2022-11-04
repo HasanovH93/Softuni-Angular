348f342a11600a19bbb43bd5982395fef9ad636b
HasanovH93 <hasanovh14@gmail.com>
2022-11-04 23:08:26 +0100

    Angular docs exercise

---
+++ lesson 1/Typescript/demo1/index.js
@@ -0,0 +1,14 @@
+let isDone = false;
+let decima = 6;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+let message = "";
+let messages = ['Message 1', 'Message 2'];
+let strictMessage = ['Message 1', 2.3];
+function sumNumbers(numbers) {
+    let result = 0;
+    for (let num of numbers) {
+        result += num;
+    }
+}
