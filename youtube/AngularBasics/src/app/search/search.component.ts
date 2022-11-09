1d15d8a001d263decb4a9c66a91fc023372ff07a
HasanovH93 <hasanovh14@gmail.com>
2022-11-09 18:29:13 +0100

    more directives exercise

--- youtube/AngularBasics/src/app/search/search.component.ts
+++ youtube/AngularBasics/src/app/search/search.component.ts
@@ -12,7 +12,7 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
   }
  
-  searchValue : string = 'Iphone';
+  searchValue : string = '';
 
   changeSearchValue(eventData: Event){
       // console.log((<HTMLInputElement>eventData.target).value);
