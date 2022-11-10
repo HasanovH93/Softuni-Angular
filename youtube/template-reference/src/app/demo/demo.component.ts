2d68b273961e35543ac46516be66bd4d0f1d0d98
HasanovH93 <hasanovh14@gmail.com>
2022-11-10 08:38:46 +0100

    reference variables

---
+++ youtube/template-reference/src/app/demo/demo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sayHello(inputElement: HTMLInputElement){
+  // alert('Hello ' + inputElement.value);
+  console.log(inputElement)
+  }
+
+}
